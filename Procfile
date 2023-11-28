ba0670a2cfd96a657ab715a1a937379a8cc5d91b
Theadr29 <adriano-almeidasantos@hotmail.com>
2023-11-28 09:47:45 -0300

    Adriano Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/com.AdrianoDev.Brazucas-all.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ application {
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
-
 repositories {
     mavenCentral()
 }
